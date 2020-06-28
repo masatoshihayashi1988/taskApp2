a654d0742dff2c5450449ade54ba9fec99ed5128
masatoshihayashi1988 <your.name.never.gone1988@gmail.com>
2020-06-28 23:18:08 +0900

    Initial Commit

---
+++ taskApp2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  taskApp2
+//
+//  Created by 林将智 on 2020/06/28.
+//  Copyright © 2020 林将智. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
